edb7e185a20d227eb2101c6169a07039467aae29
Kovalev Vladimir <wault@mail.ru>
2022-09-01 13:53:37 +0500

    requirements.txt

---
+++ ditry_main.py
@@ -0,0 +1,4 @@
+from datetime import *
+from application.salary import *
+from application.db.people import *
+
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from datetime import date
 from application.salary import calculate_salary
 from application.db.people import get_employees, employees
 
+if __name__ == '__main__':
 
-print(f'Добрый день, сегодня: {date.today()}')
+    print(f'Добрый день, сегодня: {date.today()}')
 
-print(f"Статус:{get_employees('Oleg', employees)}")
+    print(f"Статус:{get_employees('Oleg', employees)}")
 
-print(f"Сотрудник: {calculate_salary('Ivan', 5000, 22)[0]} - зарплата: {calculate_salary('Ivan', 5000, 22)[1]}")
-
+    print(f"Сотрудник: {calculate_salary('Ivan', 5000, 22)[0]} - зарплата: {calculate_salary('Ivan', 5000, 22)[1]}")
